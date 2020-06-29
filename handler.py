025bf4891ebe8d950c86a01a144fd4dcb673537e
ThiagoRochaDev <thiago.g.rocha@hotmail.com>
2020-06-29 19:13:29 -0300

    creating service

--- handler.py
+++ handler.py
@@ -2,22 +2,35 @@ import json
 import boto3
 
 
+
+
+
 def upload(event, context):
-    s3 = boto3.client('s3')
-    bucket = 'client-image-upload'
+    bucket =  "client-image-upload"
     file_name = event['Records'][0]['s3']['object']['key']
-    key_name = event['Records'][0]['s3']['object']['key']
-    s3.upload_file(file_name, bucket, key_name)    
+    s3.put_object( bucket, file_name, file_name)    
     
-def download(event, context):  
-    s3 = boto3.client('s3')
+    
+def download_process(event, context):
+    s3 = boto3.client('s3')  
+    bucket =  "client-image-upload"
     file_name = event['Records'][0]['s3']['object']['key']
-    key_name = event['Records'][0]['s3']['object']['key']
-    s3.download_file('client-image-processing', file_name, key_name)
+    s3.list_objects(bucket)
 
-
-
-def delete(event,context):      
-    s3 = boto3.client('s3')
+def download_output(event, context):  
+    bucket =  "client-image-processing"
     file_name = event['Records'][0]['s3']['object']['key']
-    s3.delete_object(Bucket='client-image-processing', Key=file_name)
+    s3.get_object(bucket, file_name)
+
+
+
+def delete_process(event,context):      
+    bucket =  "client-image-upload"
+    file_name = event['Records'][0]['s3']['object']['key']
+    s3.delete_object(bucket,file_name)
+
+
+def delete_output(event,context):      
+    bucket =  "client-image-processing"
+    file_name = event['Records'][0]['s3']['object']['key']
+    s3.delete_object(bucket,file_name)
