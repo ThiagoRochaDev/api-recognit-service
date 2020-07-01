5354febcf23b38f7740d7ea18eb68720d51a8962
ThiagoRochaDev <thiago.g.rocha@hotmail.com>
2020-07-01 01:45:25 -0300

    get functions

--- handler.py
+++ handler.py
@@ -9,17 +9,25 @@ bucket_name = 'client-image-processing'
 my_bucket = s3_resource.Bucket(bucket_name)
 
 
-def download_process(event, context):   
-    url = s3_client.generate_presigned_url('get_object',
+def download_recognized(event, context):
+    file_array = []    
+    for files in my_bucket.objects.all():
+        url = s3_client.generate_presigned_url('get_object',
                                 Params={
                                     'Bucket': 'client-image-processing',
-                                    'Key': 'destaque36.jpg',
+                                    'Key': files.key,
                                 },                                  
                                 ExpiresIn=3600)
+        file_array.append(url)
+    return {
+        "statusCode": 200,
+        "body": json.dumps(file_array)
+    }
 
+   
 
-    return url
-
+        
  
+
 
     
