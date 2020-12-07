0640baf661bc09330cb31a46b22a9e012bf92014
ThiagoRochaDev <thiago.g.rocha@hotmail.com>
2020-12-06 21:07:27 -0300

    insere imagem e pasta client no s3

--- handler.py
+++ handler.py
@@ -4,43 +4,60 @@ from botocore.client import Config
 import uuid
 import os
 from boto3.s3.transfer import TransferConfig
-
+from itertools import takewhile, count
+import base64
 
 s3_client = boto3.client('s3')
 s3_resource = boto3.resource('s3')
 dynamodb = boto3.resource('dynamodb')
 
 def client_upload(event, context):
-    with open(event['file'], "wb") as file:
+        print(event)
+        img_bin = event['body']
+        encoded_string = img_bin.encode('utf-8')
+        image = base64.b64decode(encoded_string)
+        
         bucket = 'image-bucket-upload'
-        upload_path = '/tmp/{}'.format(file)
-        response = s3_client.upload_file(file,Key=upload_path,  Bucket=bucket, Body=file)
-        # bucket_name = 'client-image-upload'
-        # my_bucket = s3_resource.Bucket(bucket_name)
-        # file_name = 'teste'
-        # object_name = 'teste.jpg'
-        # response = s3_client.upload_file(file_name, bucket_name, object_name)
+        client = event['pathParameters']['client']
+        upload_path = '%s/{}'.format(event['pathParameters']['file'] + ".jpeg") % (client)
+        response = s3_client.put_object(Key=upload_path,  Bucket=bucket, Body=image)
+        
         return {
             "statusCode": 200,
+            "headers": {
+                "status": "ok",
+                "Access-Control-Allow-Origin": "*"
+                },
             "body": json.dumps(response)
                 }
 
 def download_client_recognized(event, context):
     bucket_name = 'image-bucket-recognized'
     my_bucket = s3_resource.Bucket(bucket_name)
-    file_array = []    
+    file_array = []
+    count = 0
     for files in my_bucket.objects.all():
-        url = s3_client.generate_presigned_url('get_object',
-                                Params={
-                                    'Bucket': bucket_name,
-                                    'Key': files.key,
-                                },                                  
-                                ExpiresIn=3600)
-        file_array.append(url)
-        file_array.append(files.key)
+            count+= 1    
+            url = s3_client.generate_presigned_url('get_object',
+                                    Params={
+                                        'Bucket': bucket_name,
+                                        'Key': files.key,
+                                    },                                  
+                                    ExpiresIn=3600)
+            file_array.append({
+                        "id": uuid.uuid4().hex,
+                        "type": 'image',
+                        "thumb_url": url,
+                        "name": files.key,
+                        "position":  count  })
+        
     return {
         "statusCode": 200,
-        "body": json.dumps(file_array)
+        "headers": {
+                "status": "ok",
+                "Access-Control-Allow-Origin": "*"
+                },
+        "body":  json.dumps(file_array)
     }
 
 def download_client_upload(event, context):
@@ -58,6 +75,10 @@ def download_client_upload(event, context):
         file_array.append(files.key)
     return {
         "statusCode": 200,
+        "headers": {
+                "status": "ok",
+                "Access-Control-Allow-Origin": "*"
+                },
         "body": json.dumps(file_array)
     }   
  
@@ -76,6 +97,10 @@ def delete_client_upload(event, context):
                         )
         return {
         "statusCode": 200,
+        "headers": {
+                "status": "ok",
+                "Access-Control-Allow-Origin": "*"
+                },
         "body": json.dumps(response)
                 }   
 
